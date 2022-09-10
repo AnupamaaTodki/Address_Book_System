7be42e2dcded3ef387ddaaecab96e1427ebf2f8a
AnupamaaTodki <todkianupama@gmail.com>
2022-09-10 17:20:05 +0530

    Refracted: Added DeletePerson file to remote code base

---
+++ AddressBookMain.java
@@ -0,0 +1,70 @@
+package com.addressbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+	static Scanner scanner = new Scanner(System.in);
+	static String firstName, lastName, address, city, state, zip, phoneNumber, eamil;
+	
+	public static void readPersonDetails() {
+		System.out.print("First Name : ");
+		firstName = scanner.nextLine();
+		System.out.print("Last Name : ");
+		lastName = scanner.nextLine();
+	}
+	
+	public static void readContactDetails() {
+		readPersonDetails();
+		System.out.print("Address : ");
+		address = scanner.nextLine();
+		System.out.print("City : ");
+		city = scanner.nextLine();
+		System.out.print("State : ");
+		state = scanner.nextLine();
+		System.out.print("Zip : ");
+		zip = scanner.nextLine();
+		System.out.print("Phone Number : ");
+		phoneNumber = scanner.nextLine();
+		System.out.print("Email : ");
+		eamil = scanner.nextLine();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		
+		AddressBook addressBook = new AddressBook();
+		int indexInContact;
+		System.out.println("Enter 4 person details: ");
+		for (int number = 1; number <= 2; number++) {
+			System.out.println("");
+			System.out.println("Person "+number+" : ");
+			readContactDetails();
+			addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, eamil);	
+		}
+		addressBook.showContacts();
+		System.out.println("");
+		System.out.println("Enter first name and last name which have to edit contact :");
+		readPersonDetails();
+		indexInContact = addressBook.indexOfContact(firstName, lastName);
+		if (indexInContact != -1) {
+			System.out.println("Contact exists you can edit");
+			readContactDetails();
+			addressBook.editContact(indexInContact, firstName, lastName, address, city, state, zip, phoneNumber, address);
+		} else {
+			System.out.println("Contact not exists you can not edit");
+		}
+		addressBook.showContacts();
+		System.out.println("");
+		System.out.println("Enter first name and last name which have to delete contact :");
+		readPersonDetails();
+		indexInContact = addressBook.indexOfContact(firstName, lastName);
+		if (indexInContact != -1) {
+			System.out.println("Contact found and deleted now");
+			addressBook.deleteContact(indexInContact);
+		} else {
+			System.out.println("Contact not found so can not delete");
+		}
+		addressBook.showContacts();
+		scanner.close();
+	}
+}
