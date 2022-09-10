33907d99cae0f31741ee2c60d4237643d3f699f4
AnupamaaTodki <todkianupama@gmail.com>
2022-09-10 17:00:13 +0530

    Refracted: added file Address book to remote code base

---
+++ AddressBook.java
@@ -0,0 +1,45 @@
+package com.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+public ArrayList<Person> contacts = new ArrayList<Person>();
+	
+	public void addContact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+		contacts.add(new Person(firstName, lastName, address, city, state, zip, phoneNumber, email));
+	}
+	
+	public void showContacts() {
+		contacts.forEach((contact) -> {
+			System.out.println("");
+			System.out.println(contact.firstName);
+			System.out.println(contact.lastName);
+			System.out.println(contact.city);
+			System.out.println(contact.state);
+			System.out.println(contact.zip);
+			System.out.println(contact.phoneNumber);
+			System.out.println(contact.email);
+			System.out.println("");
+		});
+	}
+	
+	public int indexOfContact(String firstName, String lastName) {
+		for(int index = 0; index < contacts.size(); index++) {
+			Person cantact = contacts.get(index);
+			if (cantact.getFirstName().equals(firstName) && cantact.getLastName().equals(lastName))
+				return index;
+		}
+		return -1;
+	}
+	
+	public void editContact(int index, String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+		Person contact = contacts.get(index);
+		contact.setFirstName(firstName);
+		contact.setLastName(lastName);
+		contact.setCity(city);
+		contact.setState(state);
+		contact.setZip(zip);
+		contact.setPhoneNumber(phoneNumber);
+		contact.setEmail(email);
+	}
+}
